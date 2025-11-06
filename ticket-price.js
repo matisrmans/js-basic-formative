00911ba87e2188fbf6ff13625bd768a234ba7adb
user <test@gmail.com>
2025-11-06 10:24:29 +0200

    otrais uzdevums

---
+++ ticket-price.js
@@ -0,0 +1,10 @@
+let visitorAge = 12;
+let isStudent = true;
+
+if(visitorAge <= 6){
+    console.log("Ieeja bezmaksas");
+}if(isStudent == true){
+    console.log("biļetes cena ir 5 EUR");
+}else{
+    console.log("Biļetes cena ir 10 EUR");
+}
