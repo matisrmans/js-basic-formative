9792b9c422686f5d75fef83265a0ed13e75de0dd
user <test@gmail.com>
2025-11-06 10:28:33 +0200

    trešais uzdevums

---
+++ people-array.js
@@ -0,0 +1,16 @@
+const people = [
+    {
+        name: "Juris",
+        age: 32,
+        isStudent: false
+    },
+    {
+        name: "Adrians",
+        age: 19,
+        isStudent: true
+    }
+]
+
+for(person of people){
+    console.log("Vārds: " + person.name);
+}
